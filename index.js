fcfa8931ec3ba1550c7dbae0c8b8f4d512d873a7
ProfRedacted <king.uy234@gmail.com>
2024-03-22 20:47:47 +0800

    Added models and materials, can be used for testing

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const express = require('express')
+require('dotenv').config()
 
+const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.listen(3000,
-     () => console.log("Web Server is working")
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+
+app.listen(PORT,
+     () => {
+          console.log(PORT)
+          console.log("Web Server is working")
+     }
 )
 
 const mainRouter = require('./routes/main')
--- routes/material.js
+++ routes/material.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const router = express.Router()
+const Material = require('../models/material')
 
-router.get('/', (req, res) => {
-     res.status(200).json({mama:'lmao'})
+router.get('/', async (req, res) => {
+     try {
+          const materials = await Material.find()
+          res.status(200).json(materials)
+     } catch (err) {
+          res.status(500).json({message: err.message})
+     }
 })
 
-router.post('/',(req, res) => {
-     
+
+router.post('/', async (req, res) => {
+     const material = new Material({
+          material: req.body.material,
+          source: req.body.source,
+          amount: req.body.amount,
+          price: req.body.price
+     })
+     try {
+          const newMaterial = await material.save()
+          res.status(201).json(newMaterial)
+     } catch (err) {
+          res.status(400).json({message:  err.message})
+     }
 })
 
 
