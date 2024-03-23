4a38edda4afe5d6854e60f76f834b9896a4ba77d
ProfRedacted <king.uy234@gmail.com>
2024-03-23 20:26:29 +0800

    Minor Edit on index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 3000
 
 app.use(express.json())
 
+
 mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
@@ -15,8 +16,7 @@ db.once('open', () => console.log('Connected to Database'))
 
 app.listen(PORT,
      () => {
-          console.log(PORT)
-          console.log("Web Server is working")
+          console.log("Server is running on port " + PORT);
      }
 )
 
@@ -25,4 +25,5 @@ const materialRouter = require('./routes/material')
 
 app.use('/', mainRouter)
 app.use('/material', materialRouter)
+app.use
 
